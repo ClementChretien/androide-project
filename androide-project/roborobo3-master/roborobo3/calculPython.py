9c22c786eff23c0f869e9aa5fc1011d5f688d9b5
EmSavalle <emile.savalle@gmail.com>
2019-04-10 15:48:23 +0200

    Quelques calculs

--- androide-project/roborobo3-master/roborobo3/calculPython.py
+++ androide-project/roborobo3-master/roborobo3/calculPython.py
@@ -1,33 +1,38 @@
 import math
 def calcShapley():
-    nbAgent = 5
-    fName = "ResultatEval"+str(nbAgent)+".txt"
-    f1Name = "ResultatEval"+str(nbAgent+1)+".txt"
-    agent = ['R','B','C','H']
-    comb=[]
-    for r in range(nbAgent+1):
-        print("r")
-        for b in range(nbAgent+1-r):
-            for h in range(nbAgent+1-r-b):
-                c = nbAgent-r-b-h
-                comb.append( [r,b,c,h])
-    print("Read")
-    dic = getTabFile(fName)
-    dic1 = getTabFile(f1Name)
-    print("End READ")
-    for ag in range(len(agent)):
-        print(agent[ag])
-        somme = 0
-        for co in comb:
-            toFind6 = str(co[0])+"/"+str(co[1])+"/"+str(co[2])+"/"+str(co[3])
-            #cn = findValueInFile(fName,toFind6)
-            cn = int(dic[toFind6])
-            toFind7 = str(co[0]+int(ag==0))+"/"+str(co[1]+int(ag==1))+"/"+str(co[2]+int(ag==2))+"/"+str(co[3]+int(ag==3))
-            #cn1 = findValueInFile(f1Name,toFind7)
-            cn1 = int(dic1[toFind7])
-            somme+=(cn1-cn)
-        somme = somme/math.factorial(len(agent))
-        print("contribMarg de "+str(agent[ag])+" : " +str(somme))
+	for nbAgent in range(4,8):
+	    print("Nb agent : "+str(nbAgent))
+	    nbAgent = 5
+	    calcMargFile = open("CalculContribMarginale_"+str(nbAgent)+".txt","w")
+	    fName = "ResultatEval"+str(nbAgent)+".txt"
+	    f1Name = "ResultatEval"+str(nbAgent+1)+".txt"
+	    agent = ['R','B','C','H']
+	    comb=[]
+	    for r in range(nbAgent+1):
+		print("r")
+		for b in range(nbAgent+1-r):
+		    for h in range(nbAgent+1-r-b):
+		        c = nbAgent-r-b-h
+		        comb.append( [r,b,c,h])
+	    print("Read")
+	    dic = getTabFile(fName)
+	    dic1 = getTabFile(f1Name)
+	    print("End READ")
+	    for ag in range(len(agent)):
+		print(agent[ag])
+		somme = 0
+		for co in comb:
+		    toFind6 = str(co[0])+"/"+str(co[1])+"/"+str(co[2])+"/"+str(co[3])
+		    #cn = findValueInFile(fName,toFind6)
+		    cn = int(dic[toFind6])
+		    toFind7 = str(co[0]+int(ag==0))+"/"+str(co[1]+int(ag==1))+"/"+str(co[2]+int(ag==2))+"/"+str(co[3]+int(ag==3))
+		    #cn1 = findValueInFile(f1Name,toFind7)
+		    cn1 = int(dic1[toFind7])
+		    somme+=(cn1-cn)
+		somme = somme/math.factorial(len(agent))
+		print("contribMarg de "+str(agent[ag])+" : " +str(somme))
+		calcMargFile.write("contribMarg de "+str(agent[ag])+" : " +str(somme))
+	    calcMargFile.close()
 
 def getTabFile(f):
     fp = open(f,'r')
